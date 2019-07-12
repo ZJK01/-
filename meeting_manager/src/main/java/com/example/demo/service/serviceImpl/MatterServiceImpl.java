0d039a64bdc7eeae107670524d682f6c27707a6a
hygl <2374960291@qq.com>
2019-07-12 19:17:22 +0800

    第一次

---
+++ meeting_manager/src/main/java/com/example/demo/service/serviceImpl/MatterServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service.serviceImpl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.service.MatterService;
+
+@Service(value="MatterServiceImpl")
+public class MatterServiceImpl implements MatterService {
+
+}
